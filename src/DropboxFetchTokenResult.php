2e842ba6656a84c46606afdc22544857457b993d
Aleksey Romanov <lex-romanow@yandex.ru>
2025-09-28 10:09:21 +0200

    unified result handling for fetchDropboxToken method

---
+++ src/DropboxFetchTokenResult.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Dbox\UploaderApi;
+
+final class DropboxFetchTokenResult
+{
+    private bool $success;
+    private ?string $accessToken;
+    private array $error;
+
+    private function __construct(bool $success, ?string $accessToken, array $error)
+    {
+        $this->success = $success;
+        $this->accessToken = $accessToken;
+        $this->error = $error;
+    }
+
+    public static function success(string $accessToken): self
+    {
+        return new self(true, $accessToken, []);
+    }
+
+    public static function failure(array $error): self
+    {
+        return new self(false, null, $error);
+    }
+
+    public function isSuccess(): bool
+    {
+        return $this->success;
+    }
+
+    public function getAccessToken(): ?string
+    {
+        return $this->accessToken;
+    }
+
+    public function getError(): array
+    {
+        return $this->error;
+    }
+}
