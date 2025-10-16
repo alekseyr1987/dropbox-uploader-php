dc6f0e87052d73eb68968f7a40c2ea7ded464d30
Aleksey Romanov <lex-romanow@yandex.ru>
2025-10-16 21:32:42 +0200

    remove extra separator

--- src/TokenVerifier/DboxTokenVerifier.php
+++ src/TokenVerifier/DboxTokenVerifier.php
@@ -214,7 +214,7 @@ final class DboxTokenVerifier
 
         switch ($this->config['store_type']) {
             case 'local':
-                $baseDir = DIRECTORY_SEPARATOR.trim((string) $this->config['path'], DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.'dbox_uploader';
+                $baseDir = trim((string) $this->config['path'], DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR.'dbox_uploader';
                 $filePath = $baseDir.DIRECTORY_SEPARATOR.'token.json';
 
                 if (!is_dir($baseDir) && !mkdir($baseDir, 0755, true)) {
